e019ba76f6ee00ce4befe8fb4068b5ebdb2e0840
Orlando R Hernandez <47466479+acco-ohernandez@users.noreply.github.com>
2023-07-10 14:26:53 -0700

    Add project files.

---
+++ Testing_Tag_01/App.cs
@@ -0,0 +1,26 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+#endregion
+
+namespace Testing_Tag_01
+{
+    internal class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
